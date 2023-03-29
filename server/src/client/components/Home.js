6d15829bca4b018407848547a0f04d9addfdc7f3
karine <gulkarine@gmail.com>
2023-03-29 14:00:23 +0300

    chore: add watch script, update src/index.js to use the same syntax as for the client code

--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div>Home is the Best</div>
+      <button onClick={() => console.log('Hi there!')}>
+        Press me!
+      </button>
+    </div>
+  );
 };
 
 export default Home;
