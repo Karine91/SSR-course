9d43d20d3fb6b41de4348351242221a63ea5e7ef
karine <gulkarine91@gmail.com>
2023-04-06 19:39:57 +0300

    chore: added routing

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,13 @@
-import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import Home from './client/components/Home';
+import express from "express";
+import renderer from "./helpers/renderer";
 
 const app = express();
 
-app.get('/', (req, res) => {
-  const content = renderToString(<Home />);
-
-  res.send(content);
+app.use(express.static("public"));
+app.get("*", (req, res) => {
+  res.send(renderer(req));
 });
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+  console.log("Listening on port 3000");
 });
--- server/webpack.client.js
+++ server/webpack.client.js
@@ -1,23 +1,16 @@
 const path = require("path");
 const merge = require("webpack-merge");
-const webpackNodeExternals = require("webpack-node-externals");
-
 const baseConfig = require("./webpack.base");
 
 const config = {
-  // Inform webpack that we're building a bundle for node js rather than for the browser
-  target: "node",
-
   // tell webpack the root file of our server application
-  entry: "./src/index.js",
+  entry: "./src/client/client.js",
 
   // tell webpack where to put the output file that is generated
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "public"),
   },
-  // not bundle node_modules
-  externals: [webpackNodeExternals()],
 };
 
 module.exports = merge(baseConfig, config);
