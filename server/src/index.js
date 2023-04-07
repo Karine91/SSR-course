d887983d2361f9aa487deeff60d8d7a200ef846b
karine <gulkarine91@gmail.com>
2023-04-07 17:58:15 +0300

    chore: load data to store before render in server

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,23 @@
 import "babel-polyfill";
 import express from "express";
+import { matchRoutes } from "react-router-config";
+import routes from "./client/Routes";
+
 import renderer from "./helpers/renderer";
 import createStore from "./helpers/createStore";
 
 const app = express();
 
 app.use(express.static("public"));
-app.get("*", (req, res) => {
+app.get("*", async (req, res) => {
   const store = createStore();
+
+  const promises = matchRoutes(routes, req.path).map(({ route }) => {
+    return route.loadData ? route.loadData(store) : null;
+  });
+
+  await Promise.all(promises);
+
   res.send(renderer(req, store));
 });
 
